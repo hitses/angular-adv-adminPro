cb8ebb5ce3a504835acbb5b14b023542a8ab474c
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2021-02-23 12:00:54 +0100

    Progreso añadido

---
+++ src/app/components/incrementador/incrementador.component.ts
@@ -0,0 +1,42 @@
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-incrementador',
+  templateUrl: './incrementador.component.html',
+  styleUrls: ['./incrementador.component.scss']
+})
+export class IncrementadorComponent implements OnInit {
+  @Input() progreso = 50;
+  @Input() btnClass = 'info';
+
+  @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
+
+  ngOnInit(): void {
+    this.btnClass = `btn btn-${this.btnClass}`;
+  }
+
+  cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor >= 0){
+      this.valorSalida.emit(100);
+      return this.progreso = 100;
+    }
+    if (this.progreso <= 0 && valor < 0){
+      this.valorSalida.emit(0);
+      return this.progreso = 0;
+    }
+    this.progreso = this.progreso + valor;
+    this.valorSalida.emit(this.progreso);
+  }
+
+  onChange(valor: number) {
+    if (valor >= 100) {
+      this.progreso = 100;
+    } else if (valor <= 0) {
+      this.progreso = 0;
+    } else {
+      this.progreso = valor;
+    }
+
+    this.valorSalida.emit(this.progreso);
+  }
+}
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: [
-  ]
+  styleUrls: ['./progress.component.scss']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent {
+  progreso1 = 22;
+  progreso2 = 37;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get getProgreso1() {
+    return `${this.progreso1}%`;
   }
 
+  get getProgreso2() {
+    return `${this.progreso2}%`;
+  }
 }
